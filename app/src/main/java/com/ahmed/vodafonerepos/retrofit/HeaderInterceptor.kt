e5ba495f06ef9b6d43d4e5b4b4b26159f8b5f0cb
Ahmed Alaa <ahmedalaahussein00@gmail.com>
2023-12-26 22:50:19 +0200

    Remove unused code and add variant

--- app/src/main/java/com/ahmed/vodafonerepos/retrofit/HeaderInterceptor.kt
+++ app/src/main/java/com/ahmed/vodafonerepos/retrofit/HeaderInterceptor.kt
@@ -2,8 +2,6 @@ package com.ahmed.vodafonerepos.retrofit
 
 import com.ahmed.vodafonerepos.utils.Constants.Headers.ACCEPT
 import com.ahmed.vodafonerepos.utils.Constants.Headers.ACCEPT_VALUE
-import com.ahmed.vodafonerepos.utils.Constants.Headers.AUTHORIZATION
-import com.ahmed.vodafonerepos.utils.Constants.Headers.AUTHORIZATION_VALUE
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
@@ -11,18 +9,11 @@ import java.io.IOException
 
 class HeaderInterceptor : Interceptor {
 
-    init {
-        System.loadLibrary("native-lib")
-    }
-
-    external fun getAuthorizationValue(): String?
-
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
 
         val request: Request = chain.request()
             .newBuilder()
-            .addHeader(AUTHORIZATION, getAuthorizationValue() ?: AUTHORIZATION_VALUE)
             .addHeader(ACCEPT, ACCEPT_VALUE)
             .build()
         return chain.proceed(request)
